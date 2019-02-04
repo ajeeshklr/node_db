88929263ee61005213bf0921989d55afdb43d96d
ajeeshklr <ajeeshklr@gmail.com>
2019-02-04 23:02:36 +0530

    Making changes for only callback based implementation. This should be used to enable support for queueing and event based db support

--- drivers/mysql/mysql.js
+++ drivers/mysql/mysql.js
@@ -125,218 +125,167 @@ let MySQLDB = class MySQLDB extends AbstractDB {
         });
     };
 
-    insertInternal(model) {
-        var _this = this;
+    insertInternal(model, callback) {
 
-        return new Promise((resolve, reject) => {
+        this.getClientDb().query(sqlparser.convertToInsertQuery(model), (err, result) => {
 
-            _this.getClientDb().query(sqlparser.convertToInsertQuery(model), (err, result) => {
-
-                if (err) {
-                    reject(err);
-                } else {
-                    if (result.affectedRows > 0) {
-                        model.setId(result.insertId);
-                    }
-                    resolve([model]);
+            if (err) {
+                if (callback) callback(err);
+            } else {
+                if (result.affectedRows > 0) {
+                    model.setId(result.insertId);
                 }
-            });
-
-        });
-    };
-
-    deleteInternal(model) {
-
-        var _this = this;
-        return new Promise((resolve, reject) => {
-            var query = sqlparser.convertToDeleteQuery(model);
-            _this.getClientDb().query(query, (err, result) => {
-                if (err) {
-                    // console.log(err);
-                    reject(err);
-                } else {
-                    // console.log(result);
-                    if (result.affectedRows > 0) {
-                        resolve(result);
-                    } else {
-                        reject(result);
-                    }
-                }
-            });
+                if (callback) callback(null, [model]);
+            }
         });
 
     };
 
-    updateInternal(updateObject) {
+    deleteInternal(model, callback) {
+        var query = sqlparser.convertToDeleteQuery(model);
+        this.getClientDb().query(query, callback);
+    };
 
-        var _this = this;
-        return new Promise((resolve, reject) => {
+    updateInternal(updateObject, callback) {
 
-            try {
+        try {
 
-                let queryString = sqlparser.convertToUpdateQuery(updateObject);
-                if (null == queryString || queryString.length == 0) {
-                    reject("Invalid update object received.");
-                }
-
-                _this.getClientDb().query(queryString, (err, result) => {
-                    if (err) {
-                        // console.log(err);
-                        reject(err);
-                    } else {
-                        // console.log(result);
-                        if (result.affectedRows > 0) {
-                            resolve(result);
-                        } else {
-                            reject(result);
-                        }
-                    }
-                });
-
-            } catch (error) {
-                reject(error);
+            let queryString = sqlparser.convertToUpdateQuery(updateObject);
+            if (null == queryString || queryString.length == 0) {
+                reject("Invalid update object received.");
             }
 
+            this.getClientDb().query(queryString, callback);
 
-        });
+        } catch (error) {
+            if (callback) callback(error);
+        }
     };
 
-    findInternal(config) {
+    findInternal(config, callback) {
 
-        var _this = this;
-
-        return new Promise((resolve, reject) => {
-
-            /**
-             * Valid configs are as follows
-             {
-            'model' : string,   // Name of the table used in mysql db.
-            'query': {
-                 select: {
-                     "fields": ["field1,"
-                         field2 "..."
-                         fieldn
+        /**
+         * Valid configs are as follows
+         {
+        'model' : string,   // Name of the table used in mysql db.
+        'query': {
+             select: {
+                 "fields": ["field1,"
+                     field2 "..."
+                     fieldn
+                 ]
+             }
+             clause: [{
+                     "sort": [{
+                             "field": "field1",
+                             "order": "asc"
+                         },
+                         {
+                             "field": "fieldn",
+                             "order": "desc"
+                         }
                      ]
+                 },
+                 {
+                     "limit": "100" // Or a number field can be used.
+
                  }
-                 clause: [{
-                         "sort": [{
-                                 "field": "field1",
-                                 "order": "asc"
+             ]
+             filter: {
+                 "or": [{
+                         "and": [{
+                                 __op: "=",
+                                 "field1": "value1"
                              },
                              {
-                                 "field": "fieldn",
-                                 "order": "desc"
+                                 __op: "!=",
+                                 "field2": "value2"
+                             },
+                             {
+                                 __op: "<",
+                                 "field3": "value3"
+                             },
+                             {
+                                 $a: ">",
+                                 "field4": "value4"
                              }
                          ]
                      },
                      {
-                         "limit": "100" // Or a number field can be used.
-
-                     }
-                 ]
-                 filter: {
-                     "or": [{
+                         "or": {
                              "and": [{
                                      __op: "=",
-                                     "field1": "value1"
+                                     "field5": "value5"
                                  },
                                  {
                                      __op: "!=",
-                                     "field2": "value2"
-                                 },
-                                 {
-                                     __op: "<",
-                                     "field3": "value3"
-                                 },
-                                 {
-                                     $a: ">",
-                                     "field4": "value4"
+                                     "field6": "value6"
                                  }
-                             ]
-                         },
-                         {
-                             "or": {
-                                 "and": [{
-                                         __op: "=",
-                                         "field5": "value5"
-                                     },
-                                     {
-                                         __op: "!=",
-                                         "field6": "value6"
-                                     }
-                                 ],
-                                 "<": {
-                                     "field1": "100"
-                                 }
+                             ],
+                             "<": {
+                                 "field1": "100"
                              }
                          }
-                     ]
-                 }
-
+                     }
+                 ]
              }
-             * }
-             */
 
-            try {
+         }
+         * }
+         */
 
-                let query = new DbQuery(config.model, config.query.select, config.query.filter, config.query.clause);
-                let queryString = query.toSelect(); // Convert to query string.                
+        try {
 
-                _this.getClientDb().query(queryString, (err, result) => {
+            let query = new DbQuery(config.model,
+                config.query.select,
+                config.query.filter,
+                config.query.clause);
+            let queryString = query.toSelect(); // Convert to query string.                
 
-                    if (err) {
-                        // console.error(err);
-                        reject(err);
-                    } else {
-                        let models = [];
-                        let m = ModelManager.getInstance().get(config.model);
+            this.getClientDb().query(queryString, (err, result) => {
 
-                        for (var i = 0; i < result.length; i++) {
-                            let instance = new m();
-                            instance.store = this;
-                            let currentInstance = result[i];
-                            instance.beginInit();
-                            instance.getFields().forEach(field => {
-                                instance.set(field, currentInstance[field]);
-                            })
-                            instance.endInit();
-                            instance.setId(currentInstance[instance.getIdField()]);
-                            models.push(instance);
+                if (err) {
+                    if (callback) callback(err);
+                } else {
+                    let models = [];
+                    let m = ModelManager.getInstance().get(config.model);
 
-                        }
-                        // console.error(result);
-                        resolve(models);
+                    for (var i = 0; i < result.length; i++) {
+                        let instance = new m();
+                        instance.store = this;
+                        let currentInstance = result[i];
+                        instance.beginInit();
+                        instance.getFields().forEach(field => {
+                            instance.set(field, currentInstance[field]);
+                        })
+                        instance.endInit();
+                        instance.setId(currentInstance[instance.getIdField()]);
+                        models.push(instance);
+
                     }
-
-                }, this);
-
-            } catch (error) {
-                reject(error);
-            }
-        });
-    };
-
-    executeStatement(statement) {
-        return new Promise((resolve, reject) => {
-            if (typeof statement == "string") {
-                // Perform SQL query against the databse and share result once it is done.
-                try {
-                    this.getClientDb().query(queryString, (err, result) => {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            resolve(result);
-                        }
-                    });
-                } catch (ex) {
-                    console.error(ex);
-                    reject(ex);
+                    if (callback) callback(null, models);
                 }
 
-            } else {
-                reject("Invalid query statement. Exptected string, receive " + (typeof statement));
+            });
+
+        } catch (error) {
+            if (callback) callback(error);
+        }
+    };
+
+    executeStatement(statement, callback) {
+        if (typeof statement == "string") {
+            // Perform SQL query against the databse and share result once it is done.
+            try {
+                this.getClientDb().query(queryString, callback);
+            } catch (ex) {
+                console.error(ex);
+                if (callback) callback(ex);
             }
-        });
+
+        } else {
+            if (callback) callback("Invalid query statement. Exptected string, receive " + (typeof statement));
+        }
 
     }
 };
--- drivers/sqlite/sqlite_cipher.js
+++ drivers/sqlite/sqlite_cipher.js
@@ -67,139 +67,103 @@ let SqliteCipher = class SqliteCipher extends AbstractDB {
         this._sqlite3 = null;
     }
 
-    insertInternal(model) {
-
-        var _this = this;
+    insertInternal(model, callback) {
         var db = _this.getClientDb();
-
-        return new Promise((resolve, reject) => {
-            //db.serialize(function () {
-            db.run(
-                sqlparser.convertToInsertQuery(model),
-                function (err) {
-                    if (err) {
-                        console.error(err);
-                        reject(err);
-                    } else {
-                        model.setId(this.lastID);
-                        resolve(model);
-                    }
-                });
-            // });
-
-        });
-
-    }
-
-    deleteInternal(model) {
-
-        var _this = this;
-        return new Promise((resolve, reject) => {
-            var query = sqlparser.convertToDeleteQuery(model);
-            _this.getClientDb().run(query, (err) => {
+        db.run(
+            sqlparser.convertToInsertQuery(model),
+            [],
+            function (err) {
                 if (err) {
                     console.error(err);
-                    reject(err);
+                    if (callback) callback(err);
                 } else {
-                    resolve(this.changes);
+                    model.setId(this.lastID);
+                    if (callback) callback(null, model);
                 }
             });
-        });
+    }
 
+    deleteInternal(model, callback) {
+
+
+        var query = sqlparser.convertToDeleteQuery(model);
+        this.getClientDb().run(query, [], function (err) {
+            if (err) {
+                console.error(err);
+                if (callback) callback(err);
+            } else {
+                if (callback) callback(null, this.changes);
+            }
+        });
     }
 
     updateInternal(updateObject) {
-        var _this = this;
-        return new Promise((resolve, reject) => {
 
-            try {
-
-                let queryString = sqlparser.convertToUpdateQuery(updateObject);
-                if (null == queryString || queryString.length == 0) {
-                    reject("Invalid update object received.");
-                }
-
-                _this.getClientDb().query(queryString, (err, result) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        if (result.affectedRows > 0) {
-                            resolve(result);
-                        } else {
-                            reject(result);
-                        }
-                    }
-                });
-
-            } catch (error) {
-                reject(error);
+        try {
+            let queryString = sqlparser.convertToUpdateQuery(updateObject);
+            if (null == queryString || queryString.length == 0) {
+                if (callback) callback("Invalid update object received.");
             }
 
-        });
-    }
-
-    findInternal(config) {
-        var _this = this;
-        return new Promise((resolve, reject) => {
-            let query = new DbQuery(config.model, config.query.select, config.query.filter, config.query.clause);
-            let queryString = query.toSelect(); // Convert to query string.
-            query = null;
-
-            this.getClientDb().all(queryString, (err, rows) => {
+            this.getClientDb().run(queryString, [], function (err) {
                 if (err) {
-                    console.error(err);
-                    reject(err);
+                    if (callback) callback(err);
                 } else {
-                    let models = [];
-                    let m = ModelManager.getInstance().get(config.model);
-
-                    for (var i = 0; i < rows.length; i++) {
-                        let instance = new m();
-                        instance.store = this;
-                        let currentInstance = rows[i];
-                        instance.beginInit();
-                        instance.getFields().forEach(field => {
-                            instance.set(field, currentInstance[field]);
-                        })
-                        instance.endInit();
-                        instance.setId(currentInstance[instance.getIdField()]);
-                        models.push(instance);
-
-                    }
-                    resolve(models);
+                    if (callback) callback(null, this.changes);
                 }
             });
 
+        } catch (error) {
+            if (callback) callback(error);
+        }
+    }
+
+    findInternal(config, callback) {
+
+        let query = new DbQuery(config.model, config.query.select, config.query.filter, config.query.clause);
+        let queryString = query.toSelect(); // Convert to query string.
+        query = null;
+
+        this.getClientDb().all(queryString, (err, rows) => {
+            if (err) {
+                console.error(err);
+                if (callback) callback(err);
+            } else {
+                let models = [];
+                let m = ModelManager.getInstance().get(config.model);
+
+                for (var i = 0; i < rows.length; i++) {
+                    let instance = new m();
+                    instance.store = this;
+                    let currentInstance = rows[i];
+                    instance.beginInit();
+                    instance.getFields().forEach(field => {
+                        instance.set(field, currentInstance[field]);
+                    })
+                    instance.endInit();
+                    instance.setId(currentInstance[instance.getIdField()]);
+                    models.push(instance);
+
+                }
+                if (callback) callback(null, models);
+            }
         });
     }
 
     executeStatement(statement) {
         var db = this.getClientDb();
 
-        return new Promise((resolve, reject) => {
-            if (typeof statement == "string") {
-                // Perform SQL query against the databse and share result once it is done.
-                db.serialize(function () {
-                    try {
-
-                        db.run(statement, (err, result) => {
-                            if (err) {
-                                reject(err);
-                            } else {
-                                resolve(result);
-                            }
-                        });
-                    } catch (ex) {
-                        console.error(ex);
-                        reject(ex);
-                    }
+        if (typeof statement == "string") {
+            // Perform SQL query against the databse and share result once it is done.
+            try {
+                db.run(statement, [], function (err) {
+                    if (callback) callback(err, this);
                 });
-
-
-            } else {
-                reject("Invalid query statement. Exptected string, receive " + (typeof statement));
+            } catch (ex) {
+                console.error(ex);
+                if (callback) callback(ex);
             }
-        });
+        }
 
     }
 
